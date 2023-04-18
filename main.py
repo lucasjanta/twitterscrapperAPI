630cc895aab7d73f08e2f3618db83fb9edf61d20
lucasjanta <lucastyle2014@gmail.com>
2023-04-18 01:19:24 -0300

    Método reload de tweets

--- main.py
+++ main.py
@@ -17,11 +17,24 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
         tweets.append({'content': tweet.content, 'linkDrive': links, 'linkTweet': tweet.url})
 
 
-@app.get("/")
+@app.get("/tweets")
 def home():
-    return {"Tweets": len(tweets)}
+    return {tweets}
 
 
 @app.get("/tweets/{id_tweet}")
 def pegar_tweet(id_tweet: int):
     return tweets[id_tweet]
+
+@app.get("/reload/{num_tweets}")
+def recarregar_tweets(num_tweets: int):
+    limit = num_tweets
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+        if len(tweets) == limit:
+            break
+        else:
+            links = []
+            for link in tweet.links:
+                links.append(link.url)
+            tweets.append({'content': tweet.content, 'linkDrive': links, 'linkTweet': tweet.url})
+    return tweets
